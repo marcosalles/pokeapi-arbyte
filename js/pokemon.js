0c10caac287cefd0d86bb41aaef02ededdce8c8a
Marco Salles <marco.salles@geekie.com.br>
2020-04-21 20:45:47 -0300

    Selecionar pokemon

--- js/pokemon.js
+++ js/pokemon.js
@@ -6,20 +6,26 @@ document.addEventListener('DOMContentLoaded', function() {
 	axios.get('https://pokeapi.co/api/v2/pokemon?limit=251')
 		.then(response => {
 			pokemons = response.data.results;
+			var primeiroPkmn = pokemons[Math.floor(Math.random()*pokemons.length)];
 			pokemons.forEach(poke=> {
 				var split = poke.url.split('/');
 				var numero = split[split.length-2];
 				var url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/' + numero + '.png';
 				poke_imagens[poke.name] = url;
 			});
+			selecionarPokemon(primeiroPkmn.name);
 		});
 	var elems = document.querySelectorAll('.autocomplete');
 	var instances = M.Autocomplete.init(elems, { data: poke_imagens });
 });
 
-function selecionarPokemon() {
+function apertaBotao() {
 	var input = document.querySelector('#autocomplete-input');
-	axios.get(`https://pokeapi.co/api/v2/pokemon/${input.value}/`)
+	selecionarPokemon(input.value);
+}
+
+function selecionarPokemon(name) {
+	axios.get(`https://pokeapi.co/api/v2/pokemon/${name}/`)
 		.then(response => {
 			var pokemon = response.data.name;
 			document.querySelector('#nomePokemon').innerHTML = pokemon.toUpperCase();
@@ -34,20 +40,23 @@ function selecionarPokemon() {
 				.then(response =>{
 					var exibe_carousel = response.data.pokemon.slice(0,10);
 					links = exibe_carousel.map(elm => {
-						textoLink = `<a class="carousel-item" href="#"><img
-							src="${poke_imagens[elm.pokemon.name]}"></a>`
+						textoLink =
+							`<a class="carousel-item" href="#" onclick="selecionarPokemon('${elm.pokemon.name}')">
+								<img src="${poke_imagens[elm.pokemon.name]}">
+							</a>`;
 						return textoLink
 					})
-					document.querySelector('#carouselSimilares').innerHTML = links.join('')
+					document.querySelector('#carouselSimilares').innerHTML = links.join('');
+					startCarousel();
 				})
 		})
 }
 
 // Carousel
-document.addEventListener('DOMContentLoaded', function() {
+function startCarousel() {
 	var elems = document.querySelectorAll('.carousel');
-	var instances = M.Carousel.init(elems, {'fullWidth':true, numVisible: 4});
-});
+	var instances = M.Carousel.init(elems, {'fullWidth': true, numVisible: 4});
+};
 
 /*
  autocomplete
