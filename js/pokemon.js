a471f5e2def495cd4f292efdbe890aa9ece107fd
Marco Salles <marco.salles@geekie.com.br>
2020-04-21 19:00:55 -0300

    Add pokemon name and abilities

--- js/pokemon.js
+++ js/pokemon.js
@@ -1,4 +1,11 @@
+// Search
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.autocomplete');
 	var instances = M.Autocomplete.init(elems, options);
+});
+
+// Carousel
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.carousel');
+	var instances = M.Carousel.init(elems, options);
 });
