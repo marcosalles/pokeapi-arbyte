fd2424a674ebf9959b2eb5f65684a16fe250678f
Rafael Magalhaes <rafaelmbsouza@gmail.com>
2020-04-21 19:11:17 -0300

    Carrossel aparecendo mas colado na lateral

--- js/pokemon.js
+++ js/pokemon.js
@@ -7,5 +7,5 @@ document.addEventListener('DOMContentLoaded', function() {
 // Carousel
 document.addEventListener('DOMContentLoaded', function() {
 	var elems = document.querySelectorAll('.carousel');
-	var instances = M.Carousel.init(elems, options);
+	var instances = M.Carousel.init(elems, {'fullWidth':true, 'numVisible':5, 'padding':10});
 });
